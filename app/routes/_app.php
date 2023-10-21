95346e6e4695645b7aaf2e6b13746990c759c163
jacmoe <jacmoe.dk@gmail.com>
2023-10-21 23:25:09 +0200

    Removed Leaf UI - Blade is alright, for now

--- app/routes/_app.php
+++ app/routes/_app.php
@@ -1,12 +1,8 @@
 <?php
 
-use Leaf\UI;
-
-require dirname(__DIR__) . '/pages/Home/HomePage.php';
-
 app()->get('/', function () {
     /**
      * `render(view, [])` is the same as `echo view(view, [])`
      */
-    UI::render(new HomePage());
+    render('index');
 });
